d7feefe279cc054a41b8320be9892944b8302026
hdanielp <hdanielp@eservicios.indra.es>
2025-12-06 20:07:27 -0300

    feat: Commit principal de Pampero

---
+++ src/main/java/com/pamp/pampero/PamperoApplication.java
@@ -0,0 +1,13 @@
+package com.pamp.pampero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamperoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamperoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pamp/pampero/PamperoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamp.pampero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamperoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
