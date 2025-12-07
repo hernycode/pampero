8980a027762406266a26fb28cdaedf831c41e865
hdanielp <hdanielp@eservicios.indra.es>
2025-12-06 21:08:18 -0300

    feat: creación de rama inicial fase 1

--- src/main/java/com/pamp/pampero/PamperoApplication.java
+++ src/main/java/com/pamp/pampero/PamperoApplication.java
@@ -8,6 +8,7 @@ public class PamperoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PamperoApplication.class, args);
+		//modifico esta clase con comentario solo para crear la rama
 	}
 
 }
